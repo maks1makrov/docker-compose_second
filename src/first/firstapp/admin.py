9cead9edc0d427fcdc0bd23c6b20df29cb17fef7
Maks <maks1makrov@gmail.com>
2021-01-03 21:19:59 +0300

    add adminer, models

--- src/first/firstapp/admin.py
+++ src/first/firstapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from firstapp.models import A, B, C, D
+
+admin.site.register(A)
+admin.site.register(B)
+admin.site.register(C)
+admin.site.register(D)
--- src/first/firstapp/models.py
+++ src/first/firstapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class A(models.Model):
+    title = models.CharField(max_length=100)
+    b = models.ForeignKey("B", on_delete=models.CASCADE, related_name="name_b", null = True, blank=True) # "B" - т к модели еще не существует
+
+class B(models.Model):
+    title = models.CharField(max_length=100)
+    a = models.ForeignKey(A, on_delete=models.CASCADE, related_name="name_a")
+
+class C(models.Model):
+    title = models.CharField(max_length=100)
+
+class D(models.Model):
+    title = models.CharField(max_length=100)
+    c = models.ManyToManyField(C)
